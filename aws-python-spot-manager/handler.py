89b874b14a99a4761053d4682f160923e69980e1
vjcspy <mr.vjcspy@gmail.com>
2024-08-31 13:25:18 +0700

    chore: fmt

--- aws-python-spot-manager/handler.py
+++ aws-python-spot-manager/handler.py
@@ -6,9 +6,10 @@ import logging
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+
 def lambda_handler(event, context):
     ec2_client = boto3.client('ec2')
-    
+
     spot_fleet_request_id = os.getenv('SPOT_FLEET_REQUEST_ID')
     new_target_capacity = int(os.getenv('NEW_TARGET_CAPACITY'))
 
@@ -17,9 +18,9 @@ def lambda_handler(event, context):
             SpotFleetRequestId=spot_fleet_request_id,
             TargetCapacity=new_target_capacity
         )
-        
+
         logger.info(f"Response from modify_spot_fleet_request: {response}")
-        
+
         return {
             'statusCode': 200,
             'body': f'Successfully modified target capacity to {new_target_capacity}.'
@@ -27,7 +28,7 @@ def lambda_handler(event, context):
     except Exception as e:
         # Log lỗi nếu có
         logger.error(f"Error modifying target capacity: {str(e)}")
-        
+
         return {
             'statusCode': 500,
             'body': f'Error modifying target capacity: {str(e)}'
