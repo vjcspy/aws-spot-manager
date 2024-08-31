24fdbf3aa30bedb2cb665971b1028996f2c625dc
vjcspy <mr.vjcspy@gmail.com>
2024-08-31 13:14:06 +0700

    chore

--- infra/main.tf
+++ infra/main.tf
@@ -1,17 +1,17 @@
-
-
 resource "aws_iam_role" "lambda_ec2_role" {
   name = "lambda-ec2-spot-scheduler-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
-    Statement = [{
-      Action = "sts:AssumeRole",
-      Effect = "Allow",
-      Principal = {
-        Service = "lambda.amazonaws.com"
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
       }
-    }]
+    ]
   })
 }
 
